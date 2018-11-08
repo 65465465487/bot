db06d868ae986fe05ecab686e914b76bd6a67c8b
65465465487 <alks.nikita@yandex.ru>
2018-11-08 13:45:35 +0300

    Add files via upload

---
+++ heroku.py
@@ -0,0 +1,11 @@
+﻿#https://groosha.gitbooks.io/telegram-bot-lessons/content/chapter1.html
+import telebot
+TOKEN = '521423804:AAFU4SJFQzCELx0Tzos9d5G_QocZtsrY-Rw' # полученный у @BotFather
+bot = telebot.TeleBot(TOKEN)
+@bot.message_handler(commands=['start'])
+def start(message):
+    sent = bot.send_message(message.chat.id, 'Как тебя зовут?')
+    bot.register_next_step_handler(sent, hello)
+def hello(message):
+    bot.send_message(message.chat.id,'Привет, {name}! Рад тебя видеть.'.format(name=message.text))
+bot.polling()
